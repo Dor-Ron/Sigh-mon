f1591860c481a9949a7ee1933956e88c8bfd9899
Dor-Ron <dor.rondel@gmail.com>
2016-01-16 17:45:23 -0500

    minor spelling changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Sigh-mon
-Arduino simon memory game for DragonHacks 2016
+Arduino simon &copy; memory game for DragonHacks 2016
 
 Made by Alex Matson and Dor Rondel
--- sighmon.ino
+++ sighmon.ino
@@ -6,7 +6,7 @@ Sigh-mon
 AUTHORS: Alex Matson, Dor Rondel
 
 An arduino remake of of Hasbro's Simon.
-see: [Simon &copy](http://www.hasbro.com/en-us/product/_:FA45E76C-5056-9047-F541-122FDDEF7288)
+see: [Simon &copy;](http://www.hasbro.com/en-us/product/_:FA45E76C-5056-9047-F541-122FDDEF7288)
 Uses <num> amount of different colored LEDs,
 as well as buzzers, buttons, and a power-switch.
 Made at Drexel University's IEEE DragonHacks 2016.
