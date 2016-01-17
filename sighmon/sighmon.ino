d6203d269ae4ea7a526058560ac63ec9f597cfe9
alexmat2on <alexmat2on@gmail.com>
2016-01-16 23:44:57 -0500

    merge pt 3

--- sighmon/sighmon.ino
+++ sighmon/sighmon.ino
@@ -17,21 +17,64 @@ Made at Drexel University's IEEE DragonHacks 2016.
 //The LED glows when the button is pressed.
  
 char greenInputState;
-int greenButton = 2;
-int greenLed = 3;
+int greenButton = 21;
+int greenLed = 20;
 
 char yellowInputState;
-int yellowButton = 5;
-int yellowLed = 6;
+int yellowButton = 18;
+int yellowLed = 17;
 
 char redInputState;
-int redButton = 8;
-int redLed = 9;
+int redButton = 15;
+int redLed = 14;
 
 
 char blueInputState;
-int blueButton = 12;
-int blueLed = 13;
+int blueButton = 6;
+int blueLed = 7;
+
+/* MUSIC BUZZER TESTING CODE */
+#define NOTE_D0 98
+#define NOTE_D1 294
+#define NOTE_D2 330
+#define NOTE_D3 350
+#define NOTE_D4 393
+#define NOTE_D5 441
+#define NOTE_D6 495
+#define NOTE_D7 556
+#define NOTE_DL1 147
+#define NOTE_DL2 165
+#define NOTE_DL3 175
+#define NOTE_DL4 196
+#define NOTE_DL5 221
+#define NOTE_DL6 248
+#define NOTE_DL7 278
+#define NOTE_DH1 589
+#define NOTE_DH2 661
+#define NOTE_DH3 700
+#define NOTE_DH4 786
+#define NOTE_DH5 882
+#define NOTE_DH6 990
+#define NOTE_DH7 112
+ 
+#define WHOLE 1
+#define HALF 0.5
+#define QUARTER 0.25
+#define EIGHTH 0.125
+#define SIXTEENTH 0.625
+
+int tune[] =
+{
+    NOTE_D0,NOTE_D1,NOTE_D2,NOTE_D3,NOTE_D4,NOTE_D5,NOTE_D6,NOTE_D7,
+    NOTE_DL1,NOTE_DL2,NOTE_DL3,NOTE_DL4,NOTE_DL5,NOTE_DL6,NOTE_DL7,
+    NOTE_DH1,NOTE_DH2,NOTE_DH3,NOTE_DH4,NOTE_DH5,NOTE_DH6,NOTE_DH7,
+};
+
+float duration[]=
+{1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,};
+int length;   
+int tonePin=9;
+/* MUSIC BUZZER TESTING CODE */
 
 void setup()   
 {                
@@ -46,6 +89,12 @@ void setup()
 
   pinMode(blueLed, OUTPUT);
   pinMode(blueButton, INPUT);
+
+  // MUSIC TESTING STUFF
+  Serial.begin(9600);
+    pinMode(tonePin,OUTPUT);   //  initialize the digital pin as an output
+    length = sizeof(tune)/sizeof(tune[0]);
+  // MUSIC TESTING STUFF
   
 }
  
@@ -101,5 +150,14 @@ void loop()
   {
     digitalWrite(blueLed, LOW);   //Switch off LED
   }
+
+  //MUSIC TESTING STUFF
+  for(int x=1;x<length;x++)
+    {tone(tonePin,tune[x]);
+        delay(400*duration[(x%100)]);    // to distinguish the notes, set a minimum time between them.
+ 
+        noTone(tonePin); // stop the tone playing:
+    }
+  //MUSIC TESTING STUFF
 
 }
